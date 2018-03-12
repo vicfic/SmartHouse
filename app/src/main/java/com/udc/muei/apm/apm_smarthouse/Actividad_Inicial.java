a41c01c79b78208af99206dcde534d409edfd7c8
José Manuel González Núñez <keixamanu13@gmail.com>
2018-03-12 19:20:28 +0100

    Inicial

---
+++ app/src/main/java/com/udc/muei/apm/apm_smarthouse/Actividad_Inicial.java
@@ -0,0 +1,13 @@
+package com.udc.muei.apm.apm_smarthouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividad_Inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad__inicial);
+    }
+}
