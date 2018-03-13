9aa3786222ad94ef3bab43899128ed8d6fbe1131
MSI\DavidPC <d.rivas@udc.es>
2018-03-14 00:46:53 +0100

    Se añade la actividad de login con su interfaz: - Creada activity - Creada interfaz activity - Modificado strings.xml y styles.xml - Añadidos iconos para los botones - Creado toast asociado al click de los botones TODO - Crear Intent al hacer click

---
+++ app/src/main/java/com/udc/muei/apm/apm_smarthouse/activities/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.udc.muei.apm.apm_smarthouse.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.udc.muei.apm.apm_smarthouse.R;
+
+/**
+ * Created by DavidPC on 14/03/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Botón en LoginActivity Pulsado!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
