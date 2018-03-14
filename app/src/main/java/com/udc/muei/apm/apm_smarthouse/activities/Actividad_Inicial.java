211f72b3f3e48ff92adf42710fa66ab1a7d8645b
José Manuel González Núñez <keixamanu13@gmail.com>
2018-03-14 04:03:54 +0100

    - Actividad de permisos añadida. Falta por crear un adaptador para que en caso de que el usuario que esté logeado en la aplicación tenga permisos de roor, pueda modificar dichos permisos. - Añadido el botón en la actividad inicial para acceder a la actividad de permisos como un usuario logeado normal. - Añadido el botón en la actividad principal para acceder a la actividad de permisos como un usuario root.

--- app/src/main/java/com/udc/muei/apm/apm_smarthouse/activities/Actividad_Inicial.java
+++ app/src/main/java/com/udc/muei/apm/apm_smarthouse/activities/Actividad_Inicial.java
@@ -8,23 +8,65 @@ import android.widget.Button;
 
 import com.udc.muei.apm.apm_smarthouse.R;
 import com.udc.muei.apm.apm_smarthouse.activities.*;
+import com.udc.muei.apm.apm_smarthouse.model.Permiso;
+import com.udc.muei.apm.apm_smarthouse.model.Usuario;
+import com.udc.muei.apm.apm_smarthouse.model.UsuarioLight;
+
+import java.util.ArrayList;
 
 public class Actividad_Inicial extends AppCompatActivity {
 
-    Button routineList;
+    Button routineButton;
+    Button permisosJuanButton;
+    Button permisosJuanButtonROOT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad__inicial);
-    routineList = (Button) findViewById(R.id.button_routine_list);
-    routineList.setText("Rutinas");
-    routineList.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(v.getContext(), Rutinas.class);
-            startActivity(intent);
-        }
-    });
+        routineButton = (Button) findViewById(R.id.button_routine_list);
+        routineButton.setText("Rutinas");
+        routineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Rutinas.class);
+                startActivity(intent);
+            }
+        });
+
+
+        permisosJuanButton = (Button) findViewById(R.id.button_permisos_juan);
+        permisosJuanButton.setText("Permisos de Juan");
+        permisosJuanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Se simula que se selecciona un usuario en la lista de usuarios
+                UsuarioLight JuanLight = new UsuarioLight(12,"Juan");
+
+                Intent i = new Intent(v.getContext(), Permisos.class);
+                i.putExtra("usuarioLight", JuanLight);
+                i.putExtra("root", false);
+                startActivity(i);
+
+            }
+        });
+
+        permisosJuanButtonROOT = (Button) findViewById(R.id.button_permisos_juan_root);
+        permisosJuanButtonROOT.setText("Permisos de Juan (Modificables)");
+        permisosJuanButtonROOT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Se simula que se selecciona un usuario en la lista de usuarios
+                UsuarioLight JuanLight = new UsuarioLight(12,"Juan");
+
+                Intent i = new Intent(v.getContext(), Permisos.class);
+                i.putExtra("usuarioLight", JuanLight);
+                i.putExtra("root", true);
+                startActivity(i);
+
+            }
+        });
+
+
 
     }
 }
