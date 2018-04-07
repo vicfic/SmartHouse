d817174d37ad505be6fbaf4027519788f8c2ffe6
Víctor Blanco <v.blancog@udc.es>
2018-04-07 17:05:49 +0200

    Añadida ventana para poder activar el gps si está desactivado

--- app/src/main/java/com/udc/muei/apm/apm_smarthouse/activities/LoginActivity.java
+++ app/src/main/java/com/udc/muei/apm/apm_smarthouse/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -68,6 +69,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         if (!gps_enabled && !network_enabled) {
             Toast.makeText(getApplicationContext(), "El GPS está desactivado", Toast.LENGTH_LONG).show();
+            
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
         }
 
     }
